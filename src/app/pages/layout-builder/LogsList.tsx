4f7608ad957ce1b6b666a1ea1efe4af7e765b9ff
meryem <mmeryemisik@gmail.com>
2023-09-08 14:47:58 +0300

    Log sayfası dinamikleştirildi

--- src/app/pages/layout-builder/LogsList.tsx
+++ src/app/pages/layout-builder/LogsList.tsx
@@ -1,22 +1,63 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react'
-import {KTIcon} from '../../../_metronic/helpers'
+import React from 'react';
 
 type Props = {
-  className: string
-}
+  className: string;
+};
 
-const LogsList: React.FC<Props> = ({className}) => {
+type LogEntry = {
+  location: string;
+  status: string;
+  device: string;
+  ipAddress: string;
+  time: string;
+};
+
+const LogsList: React.FC<Props> = ({ className }) => {
+  const logs: LogEntry[] = [
+    {
+      location: 'USA(5)',
+      status: 'OK',
+      device: 'Chrome - Windows',
+      ipAddress: '236.125.56.78',
+      time: '2 mins ago',
+    },
+    {
+      location: 'United Kingdom(10)',
+      status: 'OK',
+      device: 'Safari - Mac OS',
+      ipAddress: '236.125.56.78',
+      time: '10 mins ago',
+    },
+    {
+      location: 'Norway(-)',
+      status: 'ERR',
+      device: 'Firefox - Windows',
+      ipAddress: '236.125.56.10',
+      time: '20 mins ago',
+    },
+    {
+      location: 'Japan(112)',
+      status: 'OK',
+      device: 'iOS - iPhone Pro',
+      ipAddress: '236.125.56.54',
+      time: '30 mins ago',
+    },
+    {
+      location: 'Italy(5)',
+      status: 'WRN',
+      device: 'Samsung Noted 5- Android',
+      ipAddress: '236.100.56.50',
+      time: '40 mins ago',
+    },
+  ];
+
   return (
     <div className={`card ${className}`}>
-      {/* begin::Header */}
       <div className='card-header border-0 pt-5'>
         <h3 className='card-title align-items-start flex-column'>
           <span className='card-label fw-bold fs-3 mb-1'>Login Sessions</span>
         </h3>
       </div>
-      {/* end::Header */}
-      {/* begin::Body */}
       <div className='card-body py-3'>
         <div className='table-responsive'>
           <table className='table align-middle table-row-bordered table-row-solid gy-4 gs-9'>
@@ -29,100 +70,30 @@ const LogsList: React.FC<Props> = ({className}) => {
                 <th className='min-w-150px'>Time</th>
               </tr>
             </thead>
-
             <tbody className='fw-6 fw-semibold text-gray-600'>
-              <tr>
-                <td>
-                  <a href='#' className='text-hover-primary text-gray-600'>
-                    USA(5)
-                  </a>
-                </td>
-
-                <td>
-                  <span className='badge badge-light-success fs-7 fw-bold'>OK</span>
-                </td>
-
-                <td>Chrome - Windows</td>
-
-                <td>236.125.56.78</td>
-
-                <td>2 mins ago</td>
-              </tr>
-              <tr>
-                <td>
-                  <a href='#' className='text-hover-primary text-gray-600'>
-                    United Kingdom(10)
-                  </a>
-                </td>
-
-                <td>
-                  <span className='badge badge-light-success fs-7 fw-bold'>OK</span>
-                </td>
-
-                <td>Safari - Mac OS</td>
-
-                <td>236.125.56.78</td>
-
-                <td>10 mins ago</td>
-              </tr>
-              <tr>
-                <td>
-                  <a href='#' className='text-hover-primary text-gray-600'>
-                    Norway(-)
-                  </a>
-                </td>
-
-                <td>
-                  <span className='badge badge-light-danger fs-7 fw-bold'>ERR</span>
-                </td>
-
-                <td>Firefox - Windows</td>
-
-                <td>236.125.56.10</td>
-
-                <td>20 mins ago</td>
-              </tr>
-              <tr>
-                <td>
-                  <a href='#' className='text-hover-primary text-gray-600'>
-                    Japan(112)
-                  </a>
-                </td>
-
-                <td>
-                  <span className='badge badge-light-success fs-7 fw-bold'>OK</span>
-                </td>
-
-                <td>iOS - iPhone Pro</td>
-
-                <td>236.125.56.54</td>
-
-                <td>30 mins ago</td>
-              </tr>
-              <tr>
-                <td>
-                  <a href='#' className='text-hover-primary text-gray-600'>
-                    Italy(5)
-                  </a>
-                </td>
-
-                <td>
-                  <span className='badge badge-light-warning fs-7 fw-bold'>WRN</span>
-                </td>
-
-                <td>Samsung Noted 5- Android</td>
-
-                <td>236.100.56.50</td>
-
-                <td>40 mins ago</td>
-              </tr>
+              {logs.map((log, index) => (
+                <tr key={index}>
+                  <td>
+                    <a href='#' className='text-hover-primary text-gray-600'>
+                      {log.location}
+                    </a>
+                  </td>
+                  <td>
+                    <span className={`badge badge-light-${log.status === 'OK' ? 'success' : log.status === 'ERR' ? 'danger' : 'warning'} fs-7 fw-bold`}>
+                      {log.status}
+                    </span>
+                  </td>
+                  <td>{log.device}</td>
+                  <td>{log.ipAddress}</td>
+                  <td>{log.time}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
       </div>
-      {/* begin::Body */}
     </div>
-  )
-}
+  );
+};
 
-export {LogsList}
+export { LogsList };
