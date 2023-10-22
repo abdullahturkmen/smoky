aca448a73b61164ce42e19c4acefd641b17af22a
Abdullah Türkmen <abdullahtrkmn13@gmail.com>
2023-10-22 18:41:09 +0300

    fdghfgdh

--- src/app/modules/templates/WidePopup.jsx
+++ src/app/modules/templates/WidePopup.jsx
@@ -9,19 +9,19 @@ function WidePopup(props) {
                     <div className="modal-dialog ">
                         <div className="modal-content overflow-hidden rounded-3">
                             <div className="position-relative">
-
-                                <button type="button" className="btn-close position-absolute end-0 m-2 bg-dark text-white d-flex justify-content-center align-items-center font-weight-bold" style={{ zIndex: '9999' }} data-bs-dismiss="modal" aria-label="Close">x</button>
+                                <button type="button" className="btn-close position-absolute end-0 m-2 pb-2 bg-dark text-white d-flex justify-content-center align-items-center font-weight-bold" style={{ zIndex: '9999' }} data-bs-dismiss="modal" aria-label="Close">x</button>
                             </div>
                             <div className="modal-body d-flex p-0">
                                 <div className='w-100'>
-                                    <div className='m-5 p-5'>
+                                    <div className='m-2 p-2 h-100 d-flex flex-column justify-content-center'>
                                         <div className='d-flex align-items-center'>
                                             <img src={props.logo} alt='' className='w-50 m-auto' />
                                         </div>
-                                        <h1 className="modal-title w-100 text-center fs-3x fw-bold">{props.title}</h1>
+                                        <h1 className="modal-title w-100 text-center fs-2x fw-bold">{props.title}</h1>
                                         <span className="modal-title text-center d-block">{props.subTitle}</span>
-
-                                        <button className='btn btn-dark w-100 mt-5'>{props.buttonText}</button>
+                                        <div className='d-flex justify-content-center'>
+                                            <button className='btn btn-dark  mt-5'>{props.buttonText}</button>
+                                        </div>
                                         <p style={{ fontSize: '10px' }} className='text-center text-muted mt-4'>{props.disclaimer}</p>
                                     </div>
                                 </div>
